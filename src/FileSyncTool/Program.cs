e9d7ed81bcc6e974fdde4154e3c6e172ebfba02f
abdulayef1 <7ka1cyr@code.edu.az>
2025-08-04 23:11:06 +0200

    feat: Add Program.cs entry point skeleton

--- src/FileSyncTool/Program.cs
+++ src/FileSyncTool/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static (string, string, int, string) LoadConfigFromFile()
+{
+    return ("","",0,"");
+}
