7cde74318a135848ca4e27f44f3b630b0fb4bad2
abdulayef1 <7ka1cyr@code.edu.az>
2025-08-04 20:55:58 +0200

    Add Synchronizer service skeleton

---
+++ src/FileSyncTool/Services/Synchronizer.cs
@@ -0,0 +1,35 @@
+﻿
+
+using FileSyncTool.Interfaces;
+
+namespace FileSyncTool.Services;
+
+public class Synchronizer
+{
+
+    private readonly string _sourcePath;
+    private readonly string _replicaPath;
+    private readonly ILogger _logger;
+
+    public Synchronizer(string sourcePath, string replicaPath, ILogger logger)
+    {
+        _sourcePath = sourcePath;
+        _replicaPath = replicaPath;
+        _logger = logger;
+    }
+
+
+    public void Run()
+    { }
+
+        private void SyncDirectories(string sourceDir, string replicaDir)
+    { }
+
+    private void DeleteOrphans(string sourceDir, string replicaDir)
+    { }
+
+    private bool FilesAreEqual(string file1, string file2) 
+    {
+        return true;
+    }
+}
